98952e023421832fa97e09c61f057833700bb05b
ebisLab <ebaiden@techie.com>
2020-05-15 11:38:43 -0400

    ring buffer class Node

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,25 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+    def __str__(self):
+        return f"{self.value}"
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.size = 0
 
     def append(self, item):
         pass
